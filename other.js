6eb4818c25c478db07a14624e24dfeedc31f48c1
Efrain Astudillo <efrain.astudillo58@gmail.com>
2017-08-14 12:49:40 -0500

    hcnaka

--- other.js
+++ other.js
@@ -3,4 +3,4 @@ var m = require('./main.js');
 console.log("Hello world!  " + m.add(2,5));
 console.log("Testing hooks in Jenkins... with an error ");
 
-nsole.log("Hello world!  " + m.add(2,5));
+console.log("Hello world!  " + m.add(2,5));
